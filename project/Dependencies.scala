4648aaafc0a25c82261bffff95d564aa18e883e6
Dan Smith <75583950+dc-smith@users.noreply.github.com>
2026-06-26 14:52:13 +0100

    DTD-4650 update dependencies (#62)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,18 +4,18 @@ object Dependencies {
 
   val test = Seq(
     "uk.gov.hmrc"            %% "performance-test-runner"   % "6.3.0"        % Test,
-    "org.playframework"      %% "play-ahc-ws-standalone"    % "3.0.10",
+    "org.playframework"      %% "play-ahc-ws-standalone"    % "3.0.13",
     "com.github.nscala-time" %% "nscala-time"               % "3.0.0",
     "org.scalaj"             %% "scalaj-http"               % "2.4.2",
     "com.github.mifmif"       % "generex"                   % "1.0.2",
-    "io.circe"               %% "circe-core"                % "0.14.15",
-    "io.circe"               %% "circe-generic"             % "0.14.15",
-    "io.circe"               %% "circe-parser"              % "0.14.15",
+    "io.circe"               %% "circe-core"                % "0.14.16",
+    "io.circe"               %% "circe-generic"             % "0.14.16",
+    "io.circe"               %% "circe-parser"              % "0.14.16",
     "com.typesafe.play"      %% "play-json"                 % "2.10.8"
 
   )
   val dependencyOverrides = Seq(
     "org.scala-lang.modules" %% "scala-parser-combinators" % "2.4.0",
-    "org.slf4j"               % "slf4j-api"                % "1.7.30"
+    "org.slf4j"               % "slf4j-api"                % "2.0.18"
   )
 }
